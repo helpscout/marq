5f38c97d71d96ffd9609a41f8b515f8f072d2f13
ItsJonQ <itsjonq@gmail.com>
2017-11-22 15:09:14 -0500

    Slugify: Fix slug handling
    This update fixes the `slugify` util function to better handle slashes. Previously, it would strip all slashes. Now, it correctly replaces slashes with `-`.

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -3,7 +3,8 @@ const slugify = slug => {
     .toString()
     .toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/[^\w\-\/]+/g, '') // Remove all non-word chars
+    .replace(/\//g, '-') // Replace all slashes with -
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, '') // Trim - from end of text
